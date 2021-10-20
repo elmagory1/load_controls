67bee9285507d560ef54ae29896e466d7ec9b79c
jiloysss <janlloydangeles@gmail.com>
2021-10-20 16:58:20 +0800

    Update

--- load_controls/doc_events/quotation.py
+++ load_controls/doc_events/quotation.py
@@ -16,7 +16,7 @@ def cancel_q(doc, event):
     for i in doc.budget_bom_reference:
 
         if i.budget_bom:
-            frappe.db.sql(""" UPDATE `tabBudget BOM` SET status=%s, quotation_canceled=1 WHERE name=%s  """,("To Quotation", i.budget_bom))
+            frappe.db.sql(""" UPDATE `tabBudget BOM` SET status=%s, quotation_cancelled=1 WHERE name=%s  """,("To Quotation", i.budget_bom))
             frappe.db.commit()
 
     for ii in doc.budget_bom_opportunity:
