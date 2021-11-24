aea69f79922d4a1e97e21e4a9f049d0394c4289f
jiloysss <janlloydangeles@gmail.com>
2021-11-24 10:11:09 +0800

    Update

--- load_controls/load_controls/doctype/budget_bom/budget_bom.js
+++ load_controls/load_controls/doctype/budget_bom/budget_bom.js
@@ -725,8 +725,8 @@ frappe.ui.form.on('Budget BOM Details', {
 	}
 });
 function compute_total_cost_expense(cur_frm) {
-    var total_cost = (cur_frm.doc.total_operation_cost + cur_frm.doc.total_additional_operation_cost + cur_frm.doc.total_raw_material_cost) - cur_frm.doc.discount_amount
-   cur_frm.doc.total_cost = ( total_cost * (parseFloat(cur_frm.doc.margin_ ? cur_frm.doc.margin_ : 0) / 100)) + total_cost
+    var total_cost = (cur_frm.doc.total_operation_cost + cur_frm.doc.total_raw_material_cost)
+   cur_frm.doc.total_cost = ( total_cost * (parseFloat(cur_frm.doc.margin_ ? cur_frm.doc.margin_ : 0) / 100))
     cur_frm.refresh_field("total_cost")
 }
 function compute_total_cost(cur_frm) {
