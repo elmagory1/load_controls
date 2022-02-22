8b6187009f17acdd3550a2f37649d327bd8da6c0
jiloysss <janlloydangeles@gmail.com>
2022-02-22 17:12:55 +0800

    Update

--- load_controls/load_controls/doctype/job_subcontor/job_subcontor.js
+++ load_controls/load_controls/doctype/job_subcontor/job_subcontor.js
@@ -2,6 +2,18 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Job Subcontor', {
+    budget_bom: function () {
+         if(cur_frm.doc.budget_bom){
+            cur_frm.call({
+              doc: cur_frm.doc,
+              method: "get_budget_bom",
+              async:false,
+              callback: function (r) {
+                  cur_frm.refresh_fields(['items','additional_cost'])
+              }
+          })
+        }
+    },
 	refresh: function(frm) {
          if(cur_frm.doc.docstatus && !cur_frm.doc.purchase_order){
                   cur_frm.add_custom_button("Purchase Order", () => {
--- load_controls/load_controls/doctype/job_subcontor/job_subcontor.py
+++ load_controls/load_controls/doctype/job_subcontor/job_subcontor.py
@@ -6,5 +6,55 @@ from frappe.model.document import Document
 
 class JobSubcontor(Document):
 	@frappe.whitelist()
+	def get_budget_bom(self):
+		bb = frappe.get_doc("Budget BOM", self.budget_bom)
+		addition_fields = ["electrical_bom_raw_material", "mechanical_bom_raw_material", "fg_sellable_bom_raw_material"]
+		for x in addition_fields:
+			for i in bb.__dict__[x]:
+				self.append("items", {
+					"item": i.item_code,
+					"item_name": i.item_name,
+					"qty": i.qty,
+					"uom": i.uom,
+					"rate": i.rate,
+					"amount": i.amount,
+				})
+		for i in bb.additional_operation_cost:
+			self.append("additional_cost", {
+				"cost_type": i.account,
+				"description": i.description,
+				"amount": i.amount,
+			})
+
+
+
+	@frappe.whitelist()
 	def generate_po(self):
-		pass
+		obj = {
+			"doctype": "Purchase Order",
+			"supplier": self.supplier,
+			"supplier_name": self.supplier_name,
+			"schedule_date": self.requried_by,
+			"items": self.get_po_items(),
+			"budget_bom_reference": [{"budget_bom": self.budget_bom}]
+		}
+		po = frappe.get_doc(obj).insert()
+		po.submit()
+		frappe.db.sql(""" UPDATE `tabProduct Change Request` SET purchase_order=%s WHERE name=%s""",
+					  (po.name, self.name))
+		frappe.db.commit()
+	@frappe.whitelist()
+	def get_po_items(self):
+		items = []
+		for i in self.items:
+			items.append({
+				"item_code": i.item,
+				"item_name": i.item_name,
+				"description": i.item_name,
+				"qty": i.qty,
+				"uom": i.uom,
+				"conversion_factor": 1,
+				"schedule_date": self.required_by,
+				"rate": i.rate,
+			})
+		return items
