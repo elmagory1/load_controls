5a472ed0029ad8daec815930b28bc02b896b2782
jiloysss <janlloydangeles@gmail.com>
2022-03-02 12:44:15 +0800

    Update

--- load_controls/doc_events/pick_list.py
+++ load_controls/doc_events/pick_list.py
@@ -8,7 +8,6 @@ def on_submit_pl(doc, method):
             woi = frappe.db.sql(""" SELECT * FROm `tabWork Order Item` WHERE name=%s""", i.work_order_item, as_dict=1)
             if len(woi) > 0:
                 frappe.db.sql(""" UPDATE `tabWork Order Item` SET picked_qty=%s WHERE name=%s """,(woi[0].picked_qty + i.qty,i.work_order_item))
-                frappe.db.sql(""" UPDATE `tabWork Order` SET material_transferred_for_manufacturing=%s WHERE name=%s """,(wo.material_transferred_for_manufacturing + 1,doc.work_order))
                 frappe.db.commit()
 
 
@@ -19,5 +18,4 @@ def on_cancel_pl(doc, method):
             woi = frappe.db.sql(""" SELECT * FROm `tabWork Order Item` WHERE name=%s""", i.work_order_item, as_dict=1)
             if len(woi) > 0:
                 frappe.db.sql(""" UPDATE `tabWork Order Item` SET picked_qty=%s WHERE name=%s """,(woi[0].picked_qty - i.qty,i.work_order_item))
-                frappe.db.sql(""" UPDATE `tabWork Order` SET material_transferred_for_manufacturing=%s WHERE name=%s """,(wo.material_transferred_for_manufacturing - 1,doc.work_order))
                 frappe.db.commit()
