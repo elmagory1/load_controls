323c4441b7173fc0954684f7f5f6536bdcd4ba60
jiloysss <janlloydangeles@gmail.com>
2022-03-02 14:05:41 +0800

    Update

--- load_controls/doc_events/job_card.py
+++ load_controls/doc_events/job_card.py
@@ -4,3 +4,9 @@ def save_job_card(doc, method):
 
     if doc.operation:
         doc.quality_inspection_required = frappe.db.get_value("Operation", doc.operation, "quality_inspection_required")
+
+
+    if doc.work_order:
+        wo = frappe.get_doc("Work Order", doc.work_order)
+        for i in wo.budget_bom_reference:
+            doc.append("budget_bom_reference",i)
